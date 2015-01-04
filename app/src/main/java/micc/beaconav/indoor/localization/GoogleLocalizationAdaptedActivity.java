2c58fe87660b296cc5eb29df42ac2242e058387b
nagash91 <nagash91@gmail.com>
2015-01-04 17:18:53 +0100

    AGGIUNTO FILE GoogleLocalizationAdaptedActivity (eliminato GoogleLocalizationAdaptedView).
    Implementato GoogleLocalizationAdapter e GoogleLocalizationAdapterView, tramite il quale semplifichiamo l'accesso alle google localization apis
    (la activity deve implementare l'interfaccia GoogleLocalizationAdapterView).

---
+++ app/src/main/java/micc/beaconav/indoor/localization/GoogleLocalizationAdaptedActivity.java
@@ -0,0 +1,13 @@
+package micc.beaconav.indoor.localization;
+
+import android.content.Context;
+import android.location.Location;
+
+/**
+ * Created by Nagash on 03/01/2015.
+ */
+public interface GoogleLocalizationAdaptedActivity
+{
+    public void onLocationChanged(Location location);
+
+}
