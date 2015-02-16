cfc4c1a8ff67781563a10032f61952f73936d63b
nagash91 <nagash91@gmail.com>
2015-02-16 16:11:10 +0100

    Implementato OnSpotMarkerSelectedListener (anche in indoorMap)

--- app/src/main/java/micc/beaconav/localization/outdoorProximity/ProximityManager.java
+++ app/src/main/java/micc/beaconav/localization/outdoorProximity/ProximityManager.java
@@ -1,6 +1,10 @@
 package micc.beaconav.localization.outdoorProximity;
 
+import android.location.Location;
+
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.LinkedList;
 
 /**
  * Created by nagash on 23/01/15.
@@ -12,31 +16,69 @@ public class ProximityManager
     private ProximityObject lastProximityObject = null;
     private ProximityObject[] proxObjects;
 
+    private int proximityRadius;
+
+    private LinkedList<ProximityObject> skimmedProxyObjects = null;
+    private Location skimmingLocation = null;
+    private int skimmingRadius;
+
+
     ProximityNotificationHandler handler;
 
     ProximityAnalysisTask task = null;
 
 
-    public ProximityManager(ProximityNotificationHandler handler, ProximityObject[] objects)
+    public ProximityManager(int proximityRadius, int skimmingRadius, ProximityNotificationHandler handler, ProximityObject[] objects)
     {
         this.handler = handler;
         this.proxObjects = objects;
+        this.proximityRadius = proximityRadius;
+        this.skimmingRadius = skimmingRadius;
     }
-    public ProximityManager(ProximityNotificationHandler handler)
+    public ProximityManager(int proximityRadius, int skimmingRadius,ProximityNotificationHandler handler)
     {
-        this.handler = handler;
-        this.proxObjects = null;
+        this(proximityRadius, skimmingRadius, handler, null);
     }
 
-    public boolean startProximityAnalysis(double myLat, double myLong, float proximityRadius, float proximitySquare)
+    public boolean startProximityAnalysis(double myLat, double myLong, float proximityRadius, float skimmingRadius)
     {
-        if(task == null && proxObjects != null)
+        Location myLoc = new Location("myLocation");
+        myLoc.setLatitude(myLat);
+        myLoc.setLongitude(myLong);
+
+        if(myLoc.distanceTo(skimmingLocation) > skimmingRadius - proximityRadius)
         {
-            task = new ProximityAnalysisTask(this, new LatLng(myLat, myLong), proxObjects);
+            makeNewSkimming(myLoc);
+        }
+
+
+        if(task == null && skimmedProxyObjects != null && skimmedProxyObjects.size() > 0)
+        {
+            task = new ProximityAnalysisTask(this, new LatLng(myLat, myLong),
+                    skimmedProxyObjects.toArray(new ProximityObject[skimmedProxyObjects.size()]));
             task.startAnalysis();
             return true;
         }
         else return false;
+    }
+
+    private void makeNewSkimming(Location newSkimmingLoc)
+    {
+        this.skimmedProxyObjects = new LinkedList<>();
+        if(proxObjects != null && proxObjects.length > 0)
+        {
+            Location loc = new Location("testLoc");
+            for(ProximityObject po : proxObjects)
+            {
+                loc.setLatitude(po.getLatitude());
+                loc.setLongitude(po.getLongitude());
+
+                if (newSkimmingLoc.distanceTo(loc) < this.skimmingRadius)
+                {
+                    skimmedProxyObjects.add(po);
+                }
+            }
+        }
     }
 
 
